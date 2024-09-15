9fe1d7b65bad84c7b9f81e869c1336f2dfcb70a4
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-09-15 21:11:39 +0100

    Create Sidebar Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/auth/login/loginPage";
 import SignUpPage from "./pages/auth/signup/signUpPage";
 import HomePage from "./pages/home/homePage";
+import Sidebar from "./components/common/Sidebar";
 
 function App() {
 	return (
 		<div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignUpPage />} />
